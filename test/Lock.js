25060a0cd3f260158b9b01e77d72d081df4ba1a5
Otti B <ottavio.braun@googlemail.com>
2025-03-04 20:25:13 +0100

    Mod adjusting tests for running

--- test/Lock.js
+++ test/Lock.js
@@ -29,13 +29,13 @@ describe("Lock", function () {
 
     it("Should receive and store the funds to lock", async function () {
       const { lock, lockedAmount } = await loadFixture(deployOneYearLockFixture);
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(BigInt(lockedAmount));
+      expect(await ethers.provider.getBalance(lock.target)).to.equal(lockedAmount);
     });
 
     it("Should fail if the unlockTime is not in the future", async function () {
       const latestTime = await time.latest();
       const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.reverted;
+      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith("Unlock time should be in the future");
     });
   });
 
@@ -43,13 +43,13 @@ describe("Lock", function () {
     describe("Validations", function () {
       it("Should revert with the right error if called too soon", async function () {
         const { lock } = await loadFixture(deployOneYearLockFixture);
-        await expect(lock.withdraw()).to.be.reverted;
+        await expect(lock.withdraw()).to.be.revertedWith("You can't withdraw yet");
       });
 
       it("Should revert with the right error if called from another account", async function () {
         const { lock, unlockTime, otherAccount } = await loadFixture(deployOneYearLockFixture);
         await time.increaseTo(unlockTime);
-        await expect(lock.connect(otherAccount).withdraw()).to.be.reverted;
+        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith("You aren't the owner");
       });
 
       it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
@@ -63,7 +63,7 @@ describe("Lock", function () {
       it("Should emit an event on withdrawals", async function () {
         const { lock, unlockTime, lockedAmount } = await loadFixture(deployOneYearLockFixture);
         await time.increaseTo(unlockTime);
-        await expect(lock.withdraw()).to.emit(lock, "Withdrawal");
+        await expect(lock.withdraw()).to.emit(lock, "Withdrawal").withArgs(lockedAmount, anyValue);
       });
     });
 
@@ -73,7 +73,7 @@ describe("Lock", function () {
         await time.increaseTo(unlockTime);
         await expect(lock.withdraw()).to.changeEtherBalances(
             [owner, lock],
-            [BigInt(lockedAmount), -BigInt(lockedAmount)]
+            [lockedAmount, -lockedAmount]
         );
       });
     });
